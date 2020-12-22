5aa9dfaf56d056471e72cfdf9faded02e85258ad
Mindy Ketchum <mindyketchum9@gmail.com>
2020-12-21 20:21:20 -0600

    js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,14 @@
 // Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
+// The data from the JSON file is named importedData
 d3.json("data/samples.json").then((importedData) => {
 
 	console.log(importedData);
 
-	var data = importedData;
+    var data = importedData;
+    
+    // Add test Subject ID No. to dropdown menus
+	var names = data.names;
+
+	names.forEach((name) => {
+		d3.select("#selDataset").append("option").text(name);
+	})
